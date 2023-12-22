ded335290618e6afee7fcf5aa8dd54cee425964e
Bayu Pratama <106969470+bayyyyu@users.noreply.github.com>
2023-12-22 19:29:58 +0700

    -

--- Course Manager App/resources/views/components/utils/delete.blade.php
+++
@@ -1,6 +0,0 @@
-
-<form action="{{$url}}" method="post" class="form-inline" onsubmit="return confirm('Yakin Menghapus Data Ini?')">
-    @csrf
-    @method("delete")
-    <button class="btn btn-danger btn-mat"><i class="feather icon-trash"></i></button>
-</form>
