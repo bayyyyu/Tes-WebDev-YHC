9c91351c1beef8338c0b0e4c30e5814aebf8b5db
Bayu Pratama <106969470+bayyyyu@users.noreply.github.com>
2023-12-22 19:31:29 +0700

    -

---
+++ Course Manager App/resources/views/components/layout/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+        <div class="mb-2 mb-md-0">
+            <span>Copyright &copy; 2023 @if (date('Y') > '2023')
+                    - {{ date('Y') }}
+                @endif Bayu Pratama</span> All rights reserved.
+            desain with ❤️ by
+            <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+        </div>
+    </div>
+</footer>
